2baa82ab6cb063f08a0e6a695cd6fdb3e8b66901
Patrick Sy <patrick.sy@get-it.us>
2020-12-20 11:41:40 +0100

    Completing extension data set

--- app/src/main/scala/mediaspyy/Data.scala
+++ app/src/main/scala/mediaspyy/Data.scala
@@ -3,5 +3,18 @@ package mediaspyy
 case class MediaData(
   title: String,
   artist: String,
-  url: String
+  album: Option[String],
+  url: String,
+  images: List[MediaImage]
+  )
+
+case class MediaImage (
+  src: String,
+  size: Option[String],
+  `type`: Option[String]
+  )
+
+case class User (
+  name: String,
+  password: String
   )
